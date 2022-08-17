297a66f3d4f31e67d48191f1d1f0eac1b231c8e7
Sergey Pozniakevich <ven07@yandex.ru>
2022-08-18 01:04:17 +0700

    chenge to Task 025

--- Task025/Program.cs
+++ Task025/Program.cs
@@ -2,25 +2,13 @@
 
 Console.Clear();
 
-// Console.WriteLine("Введите первое число: ");
-// int num1 = int.Parse(Console.ReadLine());
-// Console.WriteLine("Введите второе число: ");
-// int num2 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите первое число: ");
+int num1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int num2 = int.Parse(Console.ReadLine());
 
-// Console.WriteLine($"{num1} в степени {num2} = {Math.Pow(num1, num2)}");
+Console.WriteLine($"{num1} в степени {num2} = {Math.Pow(num1, num2)}");
 
-//Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
 
-Console.WriteLine("Введите число: ");
-int num = int.Parse(Console.ReadLine());
-
-int sum = 0;
-while (num > 0)
-{
-        sum += num % 10;
-        num /= 10;   
-}
-
-Console.WriteLine($"Сумма цифр  = {sum} ");
 
 
