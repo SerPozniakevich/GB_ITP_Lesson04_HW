dc4b6af8352f270f16f23acef0ad931f156ce98a
Sergey Pozniakevich <ven07@yandex.ru>
2022-08-18 00:56:30 +0700

    add solution Task 029

---
+++ Task029/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+Console.Clear();
+
+Console.Write("Случайный массив из 8 элементов в диапозоне [0, 99] - ");
+int i = 0;
+int coul = 0;
+Console.Write("[ ");
+while (coul < 8)
+{
+    i = new Random().Next(0, 100);
+    coul++;
+    Console.Write(i);
+    Console.Write(", ");
+    
+}
+Console.Write("]");
+   
