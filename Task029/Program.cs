b4d224f4d291c7c15b52f21ef9e8fc2f41c5fd39
Sergey Pozniakevich <ven07@yandex.ru>
2022-08-20 17:52:41 +0700

    add true solution Task 029

--- Task029/Program.cs
+++ Task029/Program.cs
@@ -1,18 +1,32 @@
 ﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
 
-Console.Clear();
+// Console.Clear();
 
-Console.Write("Случайный массив из 8 элементов в диапозоне [0, 99] - ");
-int i = 0;
-int coul = 0;
-Console.Write("[ ");
-while (coul < 8)
+// Console.Write("Случайный массив из 8 элементов в диапозоне [0, 99] - ");
+// int i = 0;
+// int coul = 0;
+// Console.Write("[ ");
+// while (coul < 8)
+// {
+//     i = new Random().Next(0, 100);
+//     coul++;
+//     Console.Write(i);
+//     Console.Write(", ");
+// }
+// Console.Write("]");
+
+//Правильный вариант
+
+int[] array = GetRandomArray(8,0,100); //"8" - количество элементов, "0" - от 0, "100" - до 100
+Console.WriteLine($"[{String.Join(",",array)}]"); // String.Join - вывод элементов через запятую
+
+int[] GetRandomArray(int size, int minValue, int maxValue)
 {
-    i = new Random().Next(0, 100);
-    coul++;
-    Console.Write(i);
-    Console.Write(", ");
-    
+    int[] result = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        result[i] = new Random().Next(minValue, maxValue);
+    }
+    return result;
 }
-Console.Write("]");
    
